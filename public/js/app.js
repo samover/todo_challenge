5baf26840bd7a168aded52fe0d80d9c74b114207
samover <samuel.overloop@gmail.com>
2015-12-05 13:36:32 +0000

    add listTasks to todoListController

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,22 @@
   var app = angular.module( 'todoList', [ 'ngResource' ] );
 
   app.controller( 'todoListController', [ function() {
+    var self = this; 
+
+    self.listTasks = {
+      "items": [
+        {
+          'title': 'A todo item',
+          'status': 0,
+          'created_at': 1449322282417
+        },
+        {
+          'title': 'A second todo item',
+          'status': 1,
+          'created_at': 1449322313457
+        }
+      ]
+    };
 
   }]);
 
--- test/todoListController.spec.js
+++ test/todoListController.spec.js
@@ -11,4 +11,23 @@ describe( 'todoListController', function() {
     expect( ctrl.newTask ).toBeUndefined();
     expect( ctrl.todoList ).toBeUndefined();
   });
+
+  describe( 'Viewing existing tasks', function() {
+    it( 'shows a list of existing tasks', function() {
+      var tasks = [
+        {
+          'title': 'A todo item',
+          'status': 0,
+          'created_at': 1449322282417
+        },
+        {
+          'title': 'A second todo item',
+          'status': 1,
+          'created_at': 1449322313457
+        }
+      ]
+
+      expect( ctrl.listTasks.items ).toEqual(tasks); 
+    });
+  });
 });
