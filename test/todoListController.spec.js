5122e14c8785d45c040b1719952b0a9a4a4cc335
samover <samuel.overloop@gmail.com>
2015-12-06 17:50:18 +0000

    unit test for count items

--- test/todoListController.spec.js
+++ test/todoListController.spec.js
@@ -20,14 +20,35 @@ describe( 'todoListController', function() {
     expect( ctrl.listTasks ).toEqual({ "items": [] });
   });
 
-  describe( 'Adding a new task' , function() {
-    it( 'adds a new task', function() {
-      ctrl.newTask.name = 'A todo item';
-      ctrl.newTask.status = 1;
-      ctrl.newTask.createdOn = 1449323087596;
-      ctrl.addTask();
-      expect( ctrl.listTasks.items ).toEqual( taskList ); 
-    });
+  it( 'adds a new task', function() {
+    ctrl.newTask.name = 'A todo item';
+    ctrl.newTask.status = 1;
+    ctrl.newTask.createdOn = 1449323087596;
+    ctrl.addTask();
+    expect( ctrl.listTasks.items ).toEqual( taskList ); 
   });
 
+  describe( 'number of tasks left', function() {
+
+    it( 'shows no number of tasks left', function() {
+      expect(ctrl.activeTasks()).toEqual(0);  
+    });
+
+    it( 'shows 2 tasks left', function() {
+      ctrl.newTask.name = 'A todo item';
+      ctrl.addTask();
+      ctrl.newTask.name = 'A second todo item';
+      ctrl.addTask();
+      expect(ctrl.activeTasks()).toEqual(2);  
+    });
+
+    it( 'shows 1 task left if second task completed', function() {
+      ctrl.newTask.name = 'A todo item';
+      ctrl.addTask();
+      ctrl.newTask.name = 'A second todo item';
+      ctrl.addTask();
+      ctrl.listTasks.items[0].status = 0;
+      expect(ctrl.activeTasks()).toEqual(1);  
+    });
+  });
 });
